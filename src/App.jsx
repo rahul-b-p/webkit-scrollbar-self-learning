3a0c11092d456c82ca51172b6ab57df5704dbae6
Rahul-BP <rahulzantoz10@lbscek.ac.in>
2024-07-07 12:08:28 +0530

    styled scrolling basics learned

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="wrapper p-2 bg-dark">
+        <div className="item bg-secondary p-4 m-4 d-flex align-items-center justify-content-center text-light">1</div>
+        <div className="item bg-secondary p-4 m-4 d-flex align-items-center justify-content-center text-light">2</div>
+        <div className="item bg-secondary p-4 m-4 d-flex align-items-center justify-content-center text-light">3</div>
+        <div className="item bg-secondary p-4 m-4 d-flex align-items-center justify-content-center text-light">4</div>
+        <div className="item bg-secondary p-4 m-4 d-flex align-items-center justify-content-center text-light">5</div>
+        <div className="item bg-secondary p-4 m-4 d-flex align-items-center justify-content-center text-light">6</div>
+        <div className="item bg-secondary p-4 m-4 d-flex align-items-center justify-content-center text-light">7</div>
+        <div className="item bg-secondary p-4 m-4 d-flex align-items-center justify-content-center text-light">8</div>
+        <div className="item bg-secondary p-4 m-4 d-flex align-items-center justify-content-center text-light">9</div>
+        <div className="item bg-secondary p-4 m-4 d-flex align-items-center justify-content-center text-light">10</div>
+        <div className="item bg-secondary p-4 m-4 d-flex align-items-center justify-content-center text-light">11</div>
+        <div className="item bg-secondary p-4 m-4 d-flex align-items-center justify-content-center text-light">12</div>
+        <div className="item bg-secondary p-4 m-4 d-flex align-items-center justify-content-center text-light">13</div>
+        <div className="item bg-secondary p-4 m-4 d-flex align-items-center justify-content-center text-light">14</div>
+        <div className="item bg-secondary p-4 m-4 d-flex align-items-center justify-content-center text-light">15</div>
+      </div>
+    </>
+  )
+}
+
+export default App
